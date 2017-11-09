10740d0d9c2b5effdd166753d14097168cbfc32e
Sofia Dandjee <sofia.dandjee@epfl.ch>
2017-11-09 23:44:18 +0100

    final

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Sofia/Desktop/Network/gtest
-# Build directory: /Users/Sofia/Desktop/Network/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
